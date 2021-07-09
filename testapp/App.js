86717575e77c3be17123171023502a776afd3b6a
dimitrix1337 <octaviopavon7@gmail.com>
2021-07-09 11:08:08 -0300

    update app.js
    background from white to orange

--- testapp/App.js
+++ testapp/App.js
@@ -14,7 +14,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#f60',
     alignItems: 'center',
     justifyContent: 'center',
   },
