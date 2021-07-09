163a6dd6f389dbe307fbe414f5332a30eb19ca53
dimitrix1337 <octaviopavon7@gmail.com>
2021-07-09 11:12:14 -0300

    styles config
    change styles name to theme and set it on another file.

---
+++ testapp/styles.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"; 
+
+const theme = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#f60',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
+  
+export { theme }
